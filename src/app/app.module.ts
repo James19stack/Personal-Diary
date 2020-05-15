126625dd60ff7ef28fa8e3856615c65451f5f429
James Mirara <mirarajames29@gmail.com>
2020-05-15 18:08:24 +0300

    Add changes made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { DiaryComponent } from './diary/diary.component';
 import { DiaryDetailsComponent } from './diary-details/diary-details.component';
 import { StrikethroughDirective } from './strikethrough.directive';
+import { GoalFormComponent } from './goal-form/goal-form.component';
+import { DiaryFormComponent } from './diary-form/diary-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiaryComponent,
     DiaryDetailsComponent,
-    StrikethroughDirective
+    StrikethroughDirective,
+    GoalFormComponent,
+    DiaryFormComponent
   ],
   imports: [
     BrowserModule,
